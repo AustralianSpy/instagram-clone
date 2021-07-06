bd7f9c6baca72c4c57f4a06b358c404e310a0c32
Sydney Workman <sworkman1996@gmail.com>
2021-07-06 00:25:24 -0500

    user sign-up config and sidebar setup

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,10 +2,14 @@ import { useState, useContext, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
+import { doesUsernameExist } from '../services/firebase';
 
-export default function Login() {
+export default function SignUp() {
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
+
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
 
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
@@ -13,21 +17,47 @@ export default function Login() {
     const [error, setError] = useState('');
     const isInvalid = password == '' || emailAddress == '';
 
-    const handleLogin = async (event) => {
+    const handleSignUp = async (event) => {
         event.preventDefault();
+        
+        const usernameExists = await doesUsernameExist(username);
+        if (!usernameExists.length) {
+            try {
+                const createdUserResult = await firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(emailAddress, password);
 
-        try {
-            await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-            history.push(ROUTES.DASHBOARD);
-        } catch (error) {
-           setEmailAddress('');
-           setPassword('');
-           setError(error.message); 
+                // authentication
+                    // -> emailAddress & password & username (displayName)
+                await createdUserResult.user.updateProfile({
+                    displayName: username
+                });
+
+                // firebase user collection (create a document)
+                await firebase.firestore().collection('users').add({
+                    userId: createdUserResult.user.uid,
+                    username: username.toLowerCase(),
+                    fullName,
+                    emailAddress: emailAddress.toLowerCase(),
+                    following: [],
+                    dateCreated: Date.now()
+                });
+
+                history.push(ROUTES.DASHBOARD);
+
+            } catch (error) {
+                setFullName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            }
+        } else {
+            setError('That username is already taken, please try another.');
         }
     };
 
     useEffect(() => {
-        document.title = 'Login - Instagram';
+        document.title = 'Sign Up - Instagram';
     }, []);
 
     return (
@@ -43,7 +73,27 @@ export default function Login() {
                 
                     {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
                 
-                    <form onSubmit={handleLogin} method="POST">
+                    <form onSubmit={handleSignUp} method="POST">
+                        <input
+                            aria-label="Enter your username"
+                            type="text"
+                            placeholder="username"
+                            className="text-sm text-gray-base w-full 
+                            mr-3 py-5 px-4 h-2 border border-gray-primary 
+                            rounded mb-2"
+                            onChange={({ target }) => setUsername(target.value)}
+                            value={username}
+                        />
+                        <input
+                            aria-label="Enter your full anme"
+                            type="text"
+                            placeholder="Full Name"
+                            className="text-sm text-gray-base w-full 
+                            mr-3 py-5 px-4 h-2 border border-gray-primary 
+                            rounded mb-2"
+                            onChange={({ target }) => setFullName(target.value)}
+                            value={fullName}
+                        />
                         <input
                             aria-label="Enter your email address"
                             type="text"
@@ -69,15 +119,15 @@ export default function Login() {
                             `bg-blue-medium text-white w-full rounded h-8 font-bold 
                             ${isInvalid && 'opacity-50'}`}
                         >
-                            Log In
+                            Sign Up
                         </button>
                     </form>
                 </div>
                 <div className="flex justify-center items-center flex-col w-full gh-white
                 p-4 rounded border border-gray-primary">
-                    <p className="text-sm">Don't have an account?{` `}
-                        <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-                            Sign Up
+                    <p className="text-sm">Have an account?{` `}
+                        <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+                            Login
                         </Link>
                     </p>
                 </div>
